8664fee5f92bcb5fbe7436ef3aa8f20b409e8dac
garidana-collab <garidana@gmail.com>
2026-02-25 00:36:56 +0900

    23 in progress
    ClassroomManager struggle

---
+++ ClassroomManager/1. ClassroomManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ClassroomManager
+{
+    const int totalStudent = 5;
+    readonly string className;  // 생성자에서 생성하기
+
+    private string[] studentNames;
+    private int currentStudent = 0;
+    private static int totalClass;
+
+    public ClassroomManager()
+    {
+        className = "1반";
+        string[] students;  // 학생 배열이 무엇?
+        totalClass += 1;
+    }
+
+    void AddStudent(string name)
+    {
+        students += name;   /// 
+        if (currentStudent > totalStudent)
+        {
+            Console.WriteLine("오류! 정원초과!");
+        }
+    }
+
+    void ShowStudent()
+    {
+
+    }
+
+    static void ShowTotalClassrooms()
+    {
+
+    }
+}
